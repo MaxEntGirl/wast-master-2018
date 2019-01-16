b169e4c5bf43a0c3d6e10ce02890427b72a15b4b
A.Vordermaier <a.vordermaier@campus.lmu.de>
2019-01-16 13:48:30 +0100

    Vergleich mit dem Lexikon: Keine Unterscheidung der Groß und Kleinschreibung verbesserte new_words_alex um ca 600 Wörter auf 5000 Wörter

--- L_lex/Vergleich mit Lexikon/extracting_new_words.py
+++ L_lex/Vergleich mit Lexikon/extracting_new_words.py
@@ -2,24 +2,25 @@ new_words = open("new_words.txt", 'w',encoding='UTF-8')
 existing_words=[]
 seq=''
 with open("witt_WAB_dela_XIX.txt", "r",encoding='UTF-8')as f:
-    lexikon=f.readlines()
-    for line in lexikon:
-        w = line.split(',')
-        w = w[0]
-        if '\\' in w:
-            w=w.split('\\')
-            w=seq.join(w)
+	lexikon=f.readlines()
+	for line in lexikon:
+		w = line.split(',')
+		w = w[0]
+		if '\\' in w:
+			w=w.split('\\')
+			w=seq.join(w)
+		w = w.lower()
+		existing_words.append(w)
 
-        existing_words.append(w)
-
-with open("nice_word_list.txt", "r",encoding='UTF-8') as l:
-    words=l.readlines()
-    i = 0
-    for w in words:
+with open("simil.txt", "r",encoding='UTF-8') as l:
+	words=l.readlines()
+	i = 0
+	for w in words:
 		w = w.replace("\n", "")
-        if w not in existing_words:
-           i += 1
-           new_words.write(w + "\n")
+		w = w.lower()
+		if w not in existing_words:
+			i += 1
+			new_words.write(w + "\n")
 
 print(i,'words are new to the lexicon.')
 
