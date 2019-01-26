9019feb6ac90e48eb68f0923cb43a4587839d707
A.Vordermaier <a.vordermaier@campus.lmu.de>
2019-01-26 16:40:42 +0100

    wordlist.py wurde durchkommentiert

--- L_lex/Wortliste/Wordlist.py
+++ L_lex/Wortliste/Wordlist.py
@@ -1,33 +1,31 @@
+#Dieses Script verwendet die gesammelten Texte in sammel.txt und extrahiert daraus eine Wortliste
+#Die Wortliste wird in die Datei Wortliste\wordlist.txt geschrieben
+
 import string
 
 wordlist = open("wordlist.txt", "w")
 textfile = open("sammel.txt", "r")
-w_list = []
+w_list = [] # Wortliste
 
 text = textfile.readlines()
 
 for line in text:
-    #print(line)
-    #tr = str.maketrans("", "", string.punctuation)
-    #line = line.translate(string.maketrans("",""), string.punctuation)
+	#Lösche alle Punktuationszeichen
     transl = string.maketrans(string.punctuation, ' '*len(string.punctuation))
     line = line.translate(transl)
-    #line = line.translate(tr)
-    #print(line)
 
     words = line.split()
 
     for word in words:
-	#change words, that are all uppercase
+	#Wörter, die komplett großgeschrieben werden, werden verändert, sodas nur der erste Buchstabe groß bleibt
 	if(word.isupper()):
-		print(word)
 		word = word.capitalize()
 	
     	if word not in w_list:
         	w_list.append(word)
 
 
-
+#Wortliste wird alphabetisch sortiert
 w_list.sort()
 
 for word in w_list:
