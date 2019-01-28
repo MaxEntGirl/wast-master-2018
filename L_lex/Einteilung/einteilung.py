5050a6cc3026a3812657b7f0e08768c0b885d651
A.Vordermaier <a.vordermaier@campus.lmu.de>
2019-01-28 16:40:22 +0100

    Órdner Einteilung durchkommentiert

--- L_lex/Einteilung/einteilung.py
+++ L_lex/Einteilung/einteilung.py
@@ -1,10 +1,16 @@
+#Dieses Script liest eine Wortliste aus Komplette_Wortliste.txt und das aktuelle Lexikon eine
+#Die Wortliste wird mit dem Lexikon verglichen
+#Wenn ein Wort bereits im Lexikon steht (Die Groß und Kleinschreibung wird nicht beachtet!), wird das Wort in die Datei unnötige_wörter.txt geschrieben
+#Der Rest in Restliste.txt
+
 unnötig = open("unnötige_wörter.txt", "w")
-final = open("finale_Wortliste.txt", "r")
-rest = open("rest.txt", "w")
+final = open("Komplette_Wortliste.txt", "r")
+rest = open("Restliste.txt", "w")
 
 lex_words=[]
 seq=''
 
+#open Lexikon
 with open("witt_WAB_dela_XIX.txt", "r",encoding='UTF-8')as lex:
     lexikon=lex.readlines()
     for line in lexikon:
@@ -16,7 +22,7 @@ with open("witt_WAB_dela_XIX.txt", "r",encoding='UTF-8')as lex:
         #w = w.lower()
         lex_words.append(w)
 
-
+#open Komplette_Wortliste.txt und vergleiche mit Lexikon
 final_words = []
 words = final.readlines()
 for line in words:
